695b05a3cbec7349237f571704e4c8a5183f18c6
joeEkstrom <57157526+joeEkstrom@users.noreply.github.com>
2022-03-12 23:27:49 -0400

    update

--- src/main/java/edu/nbcc/controllers/BookController.java
+++ src/main/java/edu/nbcc/controllers/BookController.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.el.util.Validation;
 
@@ -22,51 +24,55 @@ public class BookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String BOOK_VIEW = "/books.jsp";
 	private static final String CREATE_BOOK = "/book.jsp";
-     
+	private static final String BOOK_SUMMARY = "/bookSummary.jsp";
+
 	private RequestDispatcher view;
-	
+
 	public RequestDispatcher getView() {
 		return view;
 	}
-	
+
 	public void setView(HttpServletRequest request, String viewPath) {
 		view = request.getRequestDispatcher(viewPath);
 	}
-	
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BookController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public BookController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Book book = new Book();
-		String path = request.getPathInfo(); 
-				
-		if(path == null) {
+		String path = request.getPathInfo();
+
+		if (path == null) {
 			request.setAttribute("vm", book.getBooks());
 			setView(request, BOOK_VIEW);
-		}else {
+		} else {
 			String[] parts = path.split("/");
-			if(parts[1].equalsIgnoreCase("create") || ValidationUtil.isNumeric(parts[1])) {
+			if (parts[1].equalsIgnoreCase("create") || ValidationUtil.isNumeric(parts[1])) {
 				int id = ValidationUtil.getInteger(parts[1]);
-				if(id != 0) {
+				if (id != 0) {
 					Book bk = book.getBook(id);
-					if(bk != null) {
+					if (bk != null) {
 						BookModel vm = new BookModel();
 						vm.setBook(bk);
-						request.setAttribute("vm", bk);
-					}else {
+						request.setAttribute("vm", vm);
+					} else {
 						request.setAttribute("error", new ErrorModel("Book not found"));
 					}
+				} else {
+					request.setAttribute("vm", new BookModel());
 				}
-			}else {
+			} else {
 				request.setAttribute("vm", new BookModel());
 			}
 			setView(request, CREATE_BOOK);
@@ -75,10 +81,29 @@ public class BookController extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			List<String> errors = new ArrayList<String>();
+			if (request.getParameter("bookPrice") == null) {
+				errors.add("Book price is null");
+			}
+
+			if (request.getParameter("bookPrice") == null) {
+				errors.add("Book Term is null");
+			}
+			if (request.getParameter("bookPrice") == null) {
+				errors.add("Book Name is null");
+			}
+			
+			int id = request.getParameter("hdnBookId");
+
+		} catch (Exception ex) {
+
+		}
 	}
 
 }
