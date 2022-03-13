fd47494400da1e6f35375eebe67f80cbd2ac1e94
joeEkstrom <57157526+joeEkstrom@users.noreply.github.com>
2022-03-12 23:41:10 -0400

    update

--- src/main/java/edu/nbcc/controllers/BookController.java
+++ src/main/java/edu/nbcc/controllers/BookController.java
@@ -88,6 +88,7 @@ public class BookController extends HttpServlet {
 			throws ServletException, IOException {
 		try {
 			List<String> errors = new ArrayList<String>();
+			Book book = new Book();
 			if (request.getParameter("bookPrice") == null) {
 				errors.add("Book price is null");
 			}
@@ -99,8 +100,56 @@ public class BookController extends HttpServlet {
 				errors.add("Book Name is null");
 			}
 			
-			int id = request.getParameter("hdnBookId");
-
+			int id = ValidationUtil.getInteger(request, "hdnBookId");
+			String name = request.getParameter("bookName");
+			double price = ValidationUtil.getDouble(request,"bookPrice",errors);
+			
+			int term = ValidationUtil.getTerm(request, "bookTerm", errors);
+			
+			if(price == 0.0) {
+				errors.add("Price cannot be 0");
+			}
+			
+			if(errors.size() == 0) {
+				book.setId(id);
+				book.setName(name);
+				book.setPrice(price);
+				book.setTerm(term);
+				
+				String action = request.getParameter("action").toLowerCase();
+				
+				switch(action) {
+				case "create":
+					book = book.create();
+					request.setAttribute("createdBook", book);
+					break;
+				case "save":
+					if(book.saveBook() > 0) {
+						request.setAttribute("savedBook", book);
+					}else {
+						BookModel vm = new BookModel();
+						vm.setBook(book);
+						request.setAttribute("vm", vm);
+						request.setAttribute("error", new ErrorModel("Book does not exist to create"));
+						setView(request, CREATE_BOOK);
+					}
+					break;
+				case "delete":
+					if(book.deleteBook() > 0) {
+						request.setAttribute("deletedBook", book);
+					}else {
+						BookModel vm = new BookModel();
+						vm.setBook(book);
+						request.setAttribute("vm", vm);
+						request.setAttribute("error", new ErrorModel("Book does not exist to delete"));
+						setView(request, CREATE_BOOK);
+					}
+				}
+			}else {
+				setView(request, CREATE_BOOK);
+				request.setAttribute("error", new ErrorModel("An error has occured, Please try again"));
+			}
+			
 		} catch (Exception ex) {
 
 		}
